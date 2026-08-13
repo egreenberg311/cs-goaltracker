ab96cb2cf684d8af904b9cfe3d0b46a919ecd781
egreenberg311 <elyssa.greenberg@bonterratech.com>
2026-08-13 12:11:16 -0400

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,1 @@
+import React from 'react' import ReactDOM from 'react-dom/client' import GoalsTracker from './GoalsTracker.jsx' import './index.css' ReactDOM.createRoot(document.getElementById('root')).render( <React.StrictMode> <GoalsTracker /> </React.StrictMode>, )
