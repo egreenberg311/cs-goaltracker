6e8325b318f468c01730037759a2d517c6f67b12
egreenberg311 <elyssa.greenberg@bonterratech.com>
2026-08-13 12:44:36 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react' 
 import ReactDOM from 'react-dom/client' 
-import GoalsTracker from './GoalsTracker.jsx' 
+import GoalsTracker from './goals_tracker.jsx'
 import './index.css' 
 ReactDOM.createRoot(document.getElementById('root')).render( 
   <React.StrictMode> 
